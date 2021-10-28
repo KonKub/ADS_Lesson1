087bceb9a924606f9d14b3596832db781ffe47fb
Konstantin <iki0@yandex.ru>
2021-10-28 09:47:16 +0400

    Lesson1 project

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int X = 0;            //заданный номер числа Фибоначчи (>=0)
+
+        static int n = 0;
+        static int F0 = 0, F1 = 1;
+        static int F = 0;            //результат
+
+        static void FibonacciRecursion()
+        {
+            if (n >= X - 1) return;
+            else
+            {
+                F = F0 + F1;
+                F0 = F1;
+                F1 = F;
+
+                n++;
+                FibonacciRecursion();
+            }
+        }
+
+        static void Fibonacci()
+        {
+            for (n=0; n < X - 1; n++)
+            {
+                F = F0 + F1;
+                F0 = F1;
+                F1 = F;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            X = 5;
+
+            if (X<0)
+            {
+                Console.WriteLine($"Неверный ввод: {X}");
+                Console.ReadKey();
+                return;
+            }
+            if (X == 0)
+            {
+                F = 0;
+                Console.WriteLine($" {X} : {F}");
+                Console.ReadKey();
+                return;
+            }
+            if (X == 1)
+            {
+                F = 1;
+                Console.WriteLine($" {X} : {F}");
+                Console.ReadKey();
+                return;
+            }
+
+            F0 = 0;
+            F1 = 1;
+            F = 0;
+            FibonacciRecursion();
+            Console.WriteLine($"Рекурсия. {X} : {F}");
+
+            F0 = 0;
+            F1 = 1;
+            F = 0;
+            Fibonacci();
+            Console.WriteLine($"Без рекурсии {X} : {F}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+
+        public class TestCase
+        {
+            public int N { get; set; }
+            public bool Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        static void TestPrimeNum(TestCase testCase)
+        {
+            try
+            {
+                var actual = PrimeNum(testCase.N);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+
+        static bool PrimeNum(int number)
+        {
+            int d = 0;
+
+            for (int i = 2; i<number;i++)
+            if (number % i == 0) d++;
+
+            if (d==0)
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            int n = 0;
+            Console.Write("Введите число: ");
+            int.TryParse(Console.ReadLine(), out n);
+            if (PrimeNum(n))
+                Console.WriteLine("Простое");
+            else
+                Console.WriteLine("Не простое");
+            */
+            var testCase1 = new TestCase()
+            {
+                N = 3,
+                Expected = true,
+                ExpectedException = null
+            };
+
+            var testCase2 = new TestCase()
+            {
+                N = 5,
+                Expected = true,
+                ExpectedException = null
+            };
+
+            var testCase3 = new TestCase()
+            {
+                N = 7,
+                Expected = true,
+                ExpectedException = null
+            };
+
+            var testCase4 = new TestCase()
+            {
+                N = 6,
+                Expected = false,
+                ExpectedException = null
+            };
+            var testCase5 = new TestCase()
+            {
+                N = 10,
+                Expected = false,
+                ExpectedException = null
+            };
+            var testCase6 = new TestCase()
+            {
+                N = 100,
+                Expected = false,
+                ExpectedException = null
+            };
+
+            TestPrimeNum(testCase1);
+            TestPrimeNum(testCase2);
+            TestPrimeNum(testCase3);
+            TestPrimeNum(testCase4);
+            TestPrimeNum(testCase5);
+            TestPrimeNum(testCase6);
+
+            Console.ReadKey();
+        }
+    }
+}
+
